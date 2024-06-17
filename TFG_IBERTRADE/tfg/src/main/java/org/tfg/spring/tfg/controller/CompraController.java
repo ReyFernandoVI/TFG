b895ec27562d5139999671bdb884dda0623c9429
ReyFernandoVI <tfgreyfernandovi@gmail.com>
2024-06-18 01:28:03 +0200

    Ver producto

--- TFG_IBERTRADE/tfg/src/main/java/org/tfg/spring/tfg/controller/CompraController.java
+++ TFG_IBERTRADE/tfg/src/main/java/org/tfg/spring/tfg/controller/CompraController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class CompraController {
 
@@ -25,39 +24,46 @@ public class CompraController {
     private CarritoService carritoService;
 
     @GetMapping("/mostrarProducto")
-    public String mostrarProducto(@RequestParam("id") Long idZapatilla, ModelMap m) {
-        Zapatilla zapatilla = zapatillaService.findById(idZapatilla);
-        m.put("view", "home/mostrarProducto");
-        m.put("zapatilla",zapatilla);
-        return "_t/frame";
+    public String mostrarProducto(@RequestParam("id") Long idZapatilla, ModelMap m, HttpSession session) {
+        if (verificarSesion(session)) {
+            Zapatilla zapatilla = zapatillaService.findById(idZapatilla);
+            m.put("view", "home/mostrarProducto");
+            m.put("zapatilla", zapatilla);
+            return "_t/frame";
+        } else {
+            return "redirect:/login";
+        }
     }
 
     @PostMapping("/pay")
     public String procesarPago(
-        @RequestParam("idZapatilla") Long idZapatilla,
-        @RequestParam("cantidad") int cantidad,
-        ModelMap m) {
-        // Aquí puedes obtener los detalles de la zapatilla desde tu base de datos usando el idZapatilla
-        Zapatilla zapatilla = zapatillaService.findById(idZapatilla);
-        System.out.println(zapatilla.getNombre());
-        // Agregar los detalles de la zapatilla y la cantidad al modelo
-        m.put("zapatilla", zapatilla);
-        m.put("cantidad", cantidad);
+            @RequestParam("idZapatilla") Long idZapatilla,
+            @RequestParam("cantidad") int cantidad,
+            ModelMap m, HttpSession session) {
+        if (verificarSesion(session)) {
+            Zapatilla zapatilla = zapatillaService.findById(idZapatilla);
+            m.put("zapatilla", zapatilla);
+            m.put("cantidad", cantidad);
 
-        // Calcular el total
-        double total = zapatilla.getPrecio() * cantidad;
-        m.put("total", total);
-        m.put("view", "home/pay");
-        return "_t/frame";
-}
-
-        @GetMapping("/comprar")
-        public String getResumen(HttpSession s, ModelMap m) {
-                m.put("view", "carrito/resumen");
-                Carrito carrito = carritoService.findCarritoByUsuarioId(s);
-                m.put("carritoZapatillas",null != carrito && !carrito.getCarritoZapatillas().isEmpty() ?carrito.getCarritoZapatillas() : null);
-                return "_t/frame";
+            double total = zapatilla.getPrecio() * cantidad;
+            m.put("total", total);
+            m.put("view", "home/pay");
+            return "_t/frame";
+        } else {
+            return "redirect:/login";
         }
+    }
 
+    @GetMapping("/comprar")
+    public String getResumen(HttpSession s, ModelMap m) {
+        m.put("view", "carrito/resumen");
+        Carrito carrito = carritoService.findCarritoByUsuarioId(s);
+        m.put("carritoZapatillas", carrito != null && !carrito.getCarritoZapatillas().isEmpty() ? carrito.getCarritoZapatillas() : null);
+        return "_t/frame";
+    }
 
+    // Método para verificar la sesión
+    private boolean verificarSesion(HttpSession session) {
+        return session.getAttribute("usuario") != null;
+    }
 }
